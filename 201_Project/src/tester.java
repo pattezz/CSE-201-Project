8cba3398fe66ca73e10359960e28b64529cd10f8
pattezz <16ptessmer@gmail.com>
2018-04-30 17:30:09 -0400

    fix tester

--- 201_Project/src/tester.java
+++ 201_Project/src/tester.java
@@ -1,9 +1,15 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 
 public class tester {
-public static void main(String[] args) {
-	Alignable s = new Alignable();
-	Alignable.align("aatcg","aaootcg");
-	
+public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException  {
+//	Alignable s = new Alignable();
+//	Alignable.align("aaot","aaotcg");
+	WebScrape w = new WebScrape();
+	System.out.println(w.getFASTA("NM_000680.3"));
+//	
 }
 }
 
